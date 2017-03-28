5ad9f933e5b44a7719a491cbb46116f09016ec8a
Jafeer Ali <aboutjafeer@gmail.com>
2017-03-28 22:38:35 +0530

    httptoexpress

--- app.js
+++ app.js
@@ -1,13 +1,16 @@
-var http = require("http");
-http.createServer(function (request, response) {
-   // Send the HTTP header 
-   // HTTP Status: 200 : OK
-   // Content Type: text/plain
-   response.writeHead(200, {'Content-Type': 'text/plain'});
-   
-   // Send the response body as "Hello World"
-   response.end('Hello World\n');
-}).listen(8081);
+var express=require('express');
+var app=express();
 
-// Console will print the message
-console.log('Server running at http://127.0.0.1:8081/');
+
+
+var port = process.env.PORT || 8085;
+app.get('/', function (req, res) {
+   res.send('Hello Jafeer!!');
+});
+app.listen(port,function(err){
+  if(err){
+      console.log(err);
+  }else{
+    console.log("Server running");
+  }
+});
